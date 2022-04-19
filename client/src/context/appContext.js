d7a005559a348bf1d4c9e080604c852cc3f27c4f
Andrzej J <75665204+Jaga7@users.noreply.github.com>
2022-04-19 18:06:01 +0200

    Added Charts

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -28,6 +28,8 @@ import {
   EDIT_JOB_BEGIN,
   EDIT_JOB_SUCCESS,
   EDIT_JOB_ERROR,
+  SHOW_STATS_BEGIN,
+  SHOW_STATS_SUCCESS,
 } from './actions'
 
 import reducer from './reducer'
@@ -61,6 +63,9 @@ const initialState = {
   totalJobs: 0,
   numOfPages: 1,
   page: 1,
+  //showStats
+  stats: {},
+  monthlyApplications: [],
 }
 
 const AppContext = React.createContext()
@@ -324,6 +329,25 @@ const AppProvider = ({ children }) => {
     clearAlert()
   }
 
+  const showStats = async () => {
+    dispatch({ type: SHOW_STATS_BEGIN })
+    try {
+      const { data } = await authFetch('/jobs/stats')
+      dispatch({
+        type: SHOW_STATS_SUCCESS,
+        payload: {
+          stats: data.defaultStats,
+          monthlyApplications: data.monthlyApplications,
+        },
+      })
+    } catch (error) {
+      console.log(error.response)
+      // logoutUser()
+    }
+
+    clearAlert()
+  }
+
   return (
     <AppContext.Provider
       value={{
@@ -342,6 +366,7 @@ const AppProvider = ({ children }) => {
         setEditJob,
         deleteJob,
         editJob,
+        showStats,
       }}
     >
       {children}
